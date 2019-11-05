477a70ba3a024e263e3cf637eedb249505e6a6d1
alvinaashraf <57407049+alvinaashraf@users.noreply.github.com>
2019-11-05 22:56:02 +0500

    assignment 1

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+
+import sys
+import datetime
+
+print("Twinkle , twinkle little star, \n \t how  wonder what you are\n\t\t up above the world so high\n\t\tlike a diamond in the sky\n twinkle twinke little star\n \t how i wonder what you are ")
+
+print(sys.version)
+
+n=datetime.datetime.now()
+print(n.time)
+print(n.date)
+
+num = input('Enter a length: ')
+num2= input("enter width")
+print(num*num2)
+
+add1= input("enter number 1")
+
+add2=input("enter number 2 ")
+
+print(add1+add2)
+
+
+
+
